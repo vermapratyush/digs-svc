456b6c72c8c2fbbdd74482fef9b40d03b22f01d0
Pratyush Verma <vermapratyush@gmail.com>
2016-06-27 21:36:04 +0200

    Added debug

--- src/digs/controllers/debug.go
+++ src/digs/controllers/debug.go
@@ -11,6 +11,7 @@ func (this *DebugController) Get() {
 	debug["totalActiveClient"] = len(socket.LookUp)
 	for k, _ := range(socket.LookUp) {
 		debug[k] = 1
+		debug["push-" + k] = socket.LookUp[k].PushNotificationEnabled
 	}
 	this.Serve200(debug)
 }
